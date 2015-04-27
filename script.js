caefe3f00e9b612e31322ae7f86970456b70eff6
Michelle C. Funk <michelle@michellefunk.com>
2015-04-27 14:03:01 -0500

    Initial notes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var apikey = 'YOUR-API-KEY'; // Put your API key here
+var apikey = '8e59aef0a8bcad6ac1ccbb6c83bd9533ff6d71d5'; // Put your API key here
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
-    console.log(results);
+    console.log(results); //results is an array of objects with 1 object per search result.
 }
 
 $(document).ready(function() {
@@ -14,6 +14,7 @@ $(document).ready(function() {
 
 // HELPER FUNCTION
 // Executes a search using 'query' and runs searchCallback on the results of a success.
+
 function search(query){
 
 	$.ajax ({
@@ -31,3 +32,10 @@ function search(query){
 	});
 
 }
+//Take in search input
+//Execute search
+//Create 1 div per result - for (i = 0; i < results.length; i++) {create new bootsrap class as long as it's not > 12}
+//Display selected information in unexpanded div <div class="bootstrappy class"><div></div>
+//Display extended information in expanded div    <div style="display:none"></div></div>
+//Ensure divs are placed appropriately (bootstrap)
+//
