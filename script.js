57b8825a836b0c772f2c207e28ab7fe279270d17
Michelle C. Funk <michelle@michellefunk.com>
2015-04-27 16:50:34 -0500

    search box functionality

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ function searchCallback(results) {
     console.log(results); //results is an array of objects with 1 object per search result.
     for (i = 0; i < results.length; i++) {
         //create a div with two divs nested in it
-        $(".resultsBlock").append("<div id='result" + i + "' class='col-md-3 searchResult'><div class='preview'></div><div class='extend'></div></div>");
+        $(".resultsBlock").append("<div id='result" + i + "' class='col-md-3 searchResult'><div class='preview'></div><div style='display:none' class='extend'></div></div>");
         //div 1: image, name, original_release_date, deck
         $("#result" + i).children(".preview").append(
             "<p><img src='" + results[i].image.thumb_url + "' ></p>"
             );
         $("#result" + i).children(".preview").append(
             "<p class='gameTitle'>"+ results[i].name + "</p>");
-        //console.log("name: " + results[i].name);
         $("#result" + i).children(".preview").append(
             "<p>Release Date: "+ results[i].original_release_date + "</p>");
         $("#result" + i).children(".preview").append(
@@ -24,9 +23,15 @@ function searchCallback(results) {
         $("#result" + i).children(".extend").append(
             "<p>" + results[i].description + "</p>");
         $("#result" + i).children(".extend").append(
-            "<p>Platforms available:</p>");
+            "<p class='platforms'>Platforms available:</p>");
         for (j = 0; j < results[i].platforms.length; j++) {
-            console.log("platform " + j + ": " + results[i].platforms[j].name);
+            if (j < (results[i].platforms.length - 1)) {
+                $("#result" + i).children(".extend").append(
+                    results[i].platforms[j].name + ", ");
+            } else {
+                $("#result" + i).children(".extend").append(
+                    results[i].platforms[j].name);
+            }
         }
     }
     //Display results: results[i][image][small_url] <--Remember image tag
@@ -35,7 +40,19 @@ function searchCallback(results) {
 $(document).ready(function() {
 
 	// Start the search here!
-	search('batman');
+
+    $('.resultsBlock').on("click", ".searchResult", function() {
+        $(this).children(".extend").slideToggle();
+    });
+
+    //Text search reference: http://stackoverflow.com/questions/6524288/jquery-event-for-user-pressing-enter-in-a-textbox
+    $('.search').keyup(function(e){
+        console.log("Keyup worked at all");
+        if(e.keyCode == 13)
+        {
+            search($(this).val());
+        }
+    });
 });
 
 // HELPER FUNCTION
@@ -58,10 +75,11 @@ function search(query){
 	});
 
 }
-//Take in search input
+//TODO: Take in search input
 //Execute search
-//Create 1 div per result - for (i = 0; i < results.length; i++) {create new bootstrap class as long as it's not > 12}
-//Display selected information in unexpanded div <div class="bootstrappy class"><div></div>
-//Display extended information in expanded div    <div style="display:none"></div></div>
-//Ensure divs are placed appropriately (bootstrap)
-//
+//DONE: Create 1 div per result - for (i = 0; i < results.length; i++)
+//DONE: Display selected information in unexpanded div <div class="bootstrappy class"><div></div>
+//DONE: Display extended information in expanded div    <div style="display:none"></div></div>
+//DONE: Ensure divs are placed appropriately (bootstrap)
+//TODO: Style search results
+//TODO: Scrolling or other options for lots of text
